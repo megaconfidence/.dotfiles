747bda44d975fd98f8a9be86484b5f4a880b4298
megaconfidence <cokoghenun@gmail.com>
2025-02-25 00:14:15 +0000

    chore: Adds svelte support

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "javascript", "typescript", "python", "cpp", "c", "lua", "rust" },
+	ensure_installed = { "javascript", "typescript", "python", "cpp", "c", "lua", "rust", "svelte" },
 	indent = { enable = true, disable = { "python" } },
 	highlight = { enable = true },
 	rainbow = {
